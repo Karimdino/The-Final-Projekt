369c2f3db1af8fcb49f59156af258fbec67be33a
Christoph <christoph.snger@yahoo.com>
2022-09-02 12:01:21 +0200

    funktion slideshow

--- main.js/main.js
+++ main.js/main.js
@@ -3,15 +3,19 @@
 
 
 //++++++++++++++++++++++++++++
-
-const image = ['../pic/pic10.jpg', '../pic/pic11.jpg', '../pic/pic12.jpg', '../pic/pic13.jpg', '../pic/pic14.jpg']
-const registrationCtn = document.querySelector('.registration_ctn')
-
-function showImage(image){
-    const randomImage = Math.floor(Math.random() * image.length - 1)
-    
-
+let count = 0
+const slides = =>{
+    let j
+    let pictures = document.querySelectorAll('.slides')
+    for(j = 0; j < pictures.length; j++){
+        pictures[j].style.display = 'none'
+    }
+    count++
+    if(count > pictures.length) {count = 1}
+    pictures[count-1].style.display = 'block'
+    setTimeout("slides()", 3500)
 }
+slides()
 
 
 
